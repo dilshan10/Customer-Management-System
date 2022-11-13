a9a56dcd3210beba61390aca6f5a951bc0d4531d
dilshan10 <superdilshan999@gmail.com>
2022-11-13 12:24:07 +0530

    Add textfiled Focus

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -15,11 +17,26 @@ public class LoginFormController {
     public TextField txtPassword;
     public TextField txtUserName;
     public AnchorPane root;
+    final String user = "root";
+    final String pass = "1234";
+
+    public void GetFouceOnAction(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            txtPassword.requestFocus();
+        }
+    }
 
     public void LoginOnAction(ActionEvent actionEvent) throws IOException {
-        final String user = "root";
-        final String pass = "1234";
+        PageLoader();
+    }
 
+    public void LoginOnKeyAction(KeyEvent event) throws IOException {
+        if (event.getCode().equals(KeyCode.ENTER)){
+            PageLoader();
+        }
+    }
+
+    private void PageLoader() throws IOException {
         if (txtUserName.getText().equals(user) && txtPassword.getText().equals(pass)){
             Parent parent = FXMLLoader.load(getClass().getResource("../views/DashBord_Form.fxml"));
             Scene subScene = new Scene(parent);
@@ -30,4 +47,5 @@ public class LoginFormController {
             new Alert(Alert.AlertType.ERROR,"Please Check Your UserName And Password...!").show();
         }
     }
+
 }
